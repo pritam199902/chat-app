058113345c0ab5f4780ae2245c9535a20e7982be
Pritam Das <49154874+pritam199902@users.noreply.github.com>
2021-07-05 23:54:26 +0530

    url updated

--- src/components/socket/Socket.js
+++ src/components/socket/Socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
 
-const SERVER_URL = 'http://localhost:5000'
+const SERVER_URL = 'https://chat-app-api-001.herokuapp.com'
 const socket = io(SERVER_URL, { transport: ['websocket'] })
 
 export default socket
